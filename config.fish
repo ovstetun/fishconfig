6fe680ac7b1717a98af4f95b05e09aaba477fbf8
Trond Marius Øvstetun <trond.marius.ovstetun@nrk.no>
2018-10-09 12:52:56 +0200

    Sync setup.

--- config.fish
+++ config.fish
@@ -1,8 +1,2 @@
-# Path to Oh My Fish install.
-set -gx OMF_PATH /Users/n07074/.local/share/omf
 
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG /Users/n07074/.config/omf
-
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
